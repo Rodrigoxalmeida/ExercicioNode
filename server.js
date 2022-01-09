a54047c72ccb5909534983ddabbbcf6a7701fedc
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-01-09 00:48:38 -0300

    add pagina 404

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const path = require('path');
 
 const index = require('../tarefa_express/routes/indexRoute');
 const cadastro = require('../tarefa_express/routes/cadastroRoute');
-//const basePath = path.join(__dirname, '/public/css');
+
+const basePath = path.join(__dirname, './templates');
 
 //ler o body
 app.use(
@@ -25,6 +26,11 @@ app.use(express.static('public'));
 app.use('/cadastro', cadastro);
 app.use('/', index);
 
+
+//pagina 404
+app.use(function(req,res,next){
+    res.status(404).sendFile(`${basePath}/404.html`);
+});
 
 
 app.listen(PORT, ()=>{
